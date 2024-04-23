8fc93e05f34a0171904be4bf4b0b488f05746c82
Overload02 <alvajoy123@gmail.com>
2024-04-23 18:45:42 -0400

    initial commit

---
+++ src/main.c
@@ -0,0 +1,18 @@
+/*
+ *--------------------------------------
+ * Program Name: DENO
+ * Author: NAME
+ * License: -
+ * Description:-
+ * "Please ask for permission before any use of Authors code!"
+ *--------------------------------------
+*/
+
+#include <tice.h>
+#include <graphx.h>
+#include <keypadc.h>
+
+void main(void)
+{
+
+}
